a7c3ab915294dbdfaaffa0801f540bf866aa7f45
Zeh Fernando <zeh@zehfernando.com>
2018-08-24 23:27:27 -0400

    Added react-router and a single page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+
 import './App.css';
 
-import logo from './logo.svg';
-
-class App extends React.Component {
+export default class App extends React.Component {
 	public render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.tsx</code> and save to reload.
-				</p>
-			</div>
+			<Router>
+				<div>
+					<Route exact path="/" component={Home} />
+				</div>
+			</Router>
 		);
 	}
 }
-
-export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class Home extends React.Component {
+	public render() {
+		return (
+			<div>
+				<h2>Home</h2>
+			</div>
+		);
+	}
+}
