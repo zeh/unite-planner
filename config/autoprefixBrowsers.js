be88e08afd89e898dea9cb3894d84a48f1ba4e77
Zeh Fernando <zeh@zehfernando.com>
2018-09-02 06:17:43 -0400

    Build: autoprefix list comes from an external module

---
+++ config/autoprefixBrowsers.js
@@ -0,0 +1,10 @@
+module.exports = [
+	"ie >= 11",
+	"iOS >= 9",
+	"last 4 Firefox versions", // Last 6 months
+	"last 5 Chrome versions", // Last 6 months
+	"last 3 Opera versions", // Last 6 months
+	"Safari >= 9",
+	"last 3 Edge versions", // Last 18 months
+	"> 1%",
+];
--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -200,12 +200,7 @@ module.exports = {
 									plugins: () => [
 										require('postcss-flexbugs-fixes'),
 										autoprefixer({
-											browsers: [
-												'>1%',
-												'last 4 versions',
-												'Firefox ESR',
-												'not ie < 9', // React doesn't support IE8 anyway
-											],
+											browsers: require('./autoprefixBrowsers'),
 											flexbox: 'no-2009',
 										}),
 									],
