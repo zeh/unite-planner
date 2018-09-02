241017f9eba05f26ba82486cfc102e8efaa4a3dc
Zeh Fernando <zeh@zehfernando.com>
2018-09-02 05:18:15 -0400

    Views: schedule viewer now renders columns headers

--- src/components/schedule-day/ScheduleDay.css.d.ts
+++ src/components/schedule-day/ScheduleDay.css.d.ts
@@ -1,2 +1,7 @@
 export const main: string;
 export const title: string;
+export const sessions: string;
+export const row: string;
+export const header: string;
+export const column: string;
+export const cell: string;
--- src/components/schedule-day/ScheduleDay.tsx
+++ src/components/schedule-day/ScheduleDay.tsx
@@ -1,11 +1,21 @@
+import * as cx from 'classnames';
 import * as React from 'react';
 import { connect } from 'react-redux';
 
+import Location from '../../data/Location';
 import Schedule from '../../data/Schedule';
+import Session from "../../data/Session";
+import Topic from '../../data/Topic';
+import Track from '../../data/Track';
 import { ColumnTypes } from "../../reducers/display.reducers";
 import { IStore } from './../../models';
 
 import * as styles from './ScheduleDay.css';
+
+interface IColumnInfo {
+	name: string;
+	id: string;
+}
 
 interface IProps {
 	schedule?: Schedule;
@@ -15,6 +25,8 @@ interface IProps {
 	showTopics: boolean;
 	showTracks: boolean;
 	showSpeakers: boolean;
+	showFormat: boolean;
+	showAudience: boolean;
 	columnType: ColumnTypes;
 }
 
@@ -30,11 +42,89 @@ class ScheduleDay extends React.Component<IProps> {
 				<div className={ styles.title }>
 					{ title }
 				</div>
+				<div className={ styles.sessions }>
+					{ this.renderHeader() }
+					{ this.renderSessionRows() }
+				</div>
 			</div>
 		);
 	}
 
+	private renderHeader() {
+		const { date, schedule } = this.props;
+		const sessions = schedule ? schedule.sessions.filter((s) => s.date.getTime() === date.getTime()) : [];
+		const columns = this.getColumns(sessions);
 
+		return (
+			<div className={ cx(styles.row, styles.header) }>
+				<div key={ 'row-header' } className={ styles.column }>
+					<div className={ styles.cell }>
+						{ 'Times' }
+					</div>
+				</div>
+				{ columns.map((column) => this.renderColumnHeader(column)) }
+			</div>
+		);
+	}
+
+	private renderSessionRows() {
+		return null;
+	}
+
+	private renderColumnHeader(column: IColumnInfo) {
+		return (
+			<div key={ column.id } className={ styles.column }>
+				<div className={ styles.cell }>
+					{ column.name }
+				</div>
+			</div>
+		);
+	}
+
+	private getColumns(sessions: Session[]): IColumnInfo[] {
+		const { columnType } = this.props;
+		switch (columnType) {
+			case ColumnTypes.LOCATION:
+				return this.getLocationsFromSessions(sessions);
+			case ColumnTypes.TOPIC:
+				return this.getTopicsFromSessions(sessions);
+			case ColumnTypes.TRACK:
+				return this.getTracksFromSessions(sessions);
+			default:
+				return [];
+		}
+	}
+
+	private getLocationsFromSessions(sessions: Session[]): Location[] {
+		return sessions.reduce((locations: Location[], session) => {
+			if (!locations.find((l) => l === session.location)) {
+				locations.push(session.location);
+			}
+			return locations;
+		}, []);
+	}
+
+	private getTopicsFromSessions(sessions: Session[]): Topic[] {
+		return sessions.reduce((topics: Topic[], session) => {
+			session.topics.forEach((topic) => {
+				if (!topics.find((t) => t === topic)) {
+					topics.push(topic);
+				}
+			});
+			return topics;
+		}, []);
+	}
+
+	private getTracksFromSessions(sessions: Session[]): Track[] {
+		return sessions.reduce((tracks: Track[], session) => {
+			session.tracks.forEach((track) => {
+				if (!tracks.find((t) => t === track)) {
+					tracks.push(track);
+				}
+			});
+			return tracks;
+		}, []);
+	}
 }
 
 const mapStateToProps = (state: IStore) => ({
@@ -43,6 +133,8 @@ const mapStateToProps = (state: IStore) => ({
 	showTopics: state.displayState.showTopics,
 	showTracks: state.displayState.showTracks,
 	showSpeakers: state.displayState.showSpeakers,
+	showFormat: state.displayState.showFormat,
+	showAudience: state.displayState.showAudience,
 	columnType: state.displayState.columnType,
 });
 
