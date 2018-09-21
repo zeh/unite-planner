40ba36ee625112300a4872a658a44bf9de2250ad
Zeh Fernando <zeh@zehfernando.com>
2018-09-20 22:43:20 -0400

    Feature: added persistence to the display options via redux-persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import Root from './pages/Root';
-import store from './store';
+import { persistor, store } from './store';
 
 export default class App extends React.Component {
 	public render() {
 		return (
-			<Provider store={store}>
-				<Router>
-					<Route
-						path={ "/" }
-						component={ Root }
-					/>
-				</Router>
+			<Provider store={ store }>
+				<PersistGate loading={ null } persistor={ persistor }>
+					<Router>
+						<Route
+							path={ "/" }
+							component={ Root }
+						/>
+					</Router>
+				</PersistGate>
 			</Provider>
 		);
 	}
--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,24 @@
-import { applyMiddleware, createStore } from 'redux'
-import createSagaMiddleware from 'redux-saga'
+import { applyMiddleware, createStore } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import createSagaMiddleware from 'redux-saga';
 
 import reducers from './reducers';
 import sagas from './sagas'
 
-const sagaMiddleware = createSagaMiddleware()
-const store = createStore(
-	reducers,
-	applyMiddleware(sagaMiddleware)
-)
-sagaMiddleware.run(sagas)
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: [ "displayState" ],
+};
 
-export default store;
+const persistedReducers = persistReducer(persistConfig, reducers)
+
+const sagaMiddleware = createSagaMiddleware();
+export const store = createStore(
+	persistedReducers,
+	applyMiddleware(sagaMiddleware)
+);
+export const persistor = persistStore(store);
+
+sagaMiddleware.run(sagas);
